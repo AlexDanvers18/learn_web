d2bcd466a3dc676af7cb5dc6c82f47eea6e9318e
Alex Vechnaya <alexandra.vekoveshnikova@yandex.ru>
2024-03-30 09:41:15 +0300

    Обновили, добавили связь с БД

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from webapp.model import db, News
 import requests
 from bs4 import BeautifulSoup
 
@@ -21,17 +23,26 @@ def get_python_news(html):
             title = news.find('a').text
             url = news.find('a')['href']
             published = news.find('time').text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
+            try:
+                published = datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published = datetime.now()
+            save_news(title=title, url=url, published=published)
+        #     result_news.append({
+        #         "title": title,
+        #         "url": url,
+        #         "published": published
+        #     })
+        # return result_news
     return False
     
 
-
-
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
 
 # def get_python_news(html):
 #     html = get_html("https://www.python.org/blogs/")
